8ee098a82081a75f282352b0c1bc3a0fa9fd0a49
windows 10 <windows 10@DESKTOP-EF8TPFQ>
2023-11-03 19:05:30 -0700

    Add project files.

---
+++ ProjectShop_WebApi/Controllers/ShopController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ProjectShop_WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ShopController : ControllerBase
+    {
+        [Route("GetName")]
+        [HttpGet]
+        public async Task<string> GetName()
+        {
+            return "فروشگاه";
+        }
+
+        [Route("GetListProduct")]
+        [HttpGet]
+        public async Task<List<ListProduct>> GetListProduct()
+        {
+            // مثلا از دیتابیس داره فچ میکنه
+            List<ListProduct> List = new List<ListProduct>();
+            List.Add(new ListProduct { Id = 1, Code = 10001, Name = "جارو برقی", Price = 1200000 });
+            List.Add(new ListProduct { Id = 2, Code = 10002, Name = " اتو ", Price = 500000 });
+            List.Add(new ListProduct { Id = 3, Code = 10003, Name = " لباسشویی", Price = 2200000 });
+            List.Add(new ListProduct { Id = 4, Code = 10004, Name = "تلویزیون", Price = 3200000 });
+
+            return List;
+        }
+
+        //RestFull Api
+        [Route("GetListProductByCode/{code}")]
+        [HttpGet]
+        public async Task<ListProduct> GetListProductByCode(long? Code)
+        {
+            // مثلا از دیتابیس داره فچ میکنه
+            List<ListProduct>? List = new List<ListProduct>();
+            List.Add(new ListProduct { Id = 1, Code = 10001, Name = "جارو برقی", Price = 1200000 });
+            List.Add(new ListProduct { Id = 2, Code = 10002, Name = " اتو ", Price = 500000 });
+            List.Add(new ListProduct { Id = 3, Code = 10003, Name = " لباسشویی", Price = 2200000 });
+            List.Add(new ListProduct { Id = 4, Code = 10004, Name = "تلویزیون", Price = 3200000 });
+
+            var Find = List.Where(x => x.Code == Code).FirstOrDefault();
+            return Find;
+        }
+
+    }
+}
---
+++ ProjectShop_WebApi/ListProduct.cs
@@ -0,0 +1,13 @@
+﻿namespace ProjectShop_WebApi
+{
+    public class ListProduct
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public long Code { get; set; }
+
+        public double Price { get; set; }
+
+
+    }
+}
