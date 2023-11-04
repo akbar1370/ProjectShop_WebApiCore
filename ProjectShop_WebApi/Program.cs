7af941f72b99c9816571c6a180f626c48484f696
akbar1370 <mahanakbar12001200@gmail.com>
2023-11-03 19:49:25 -0700

    Allow Policy Core

--- ProjectShop_WebApi/Program.cs
+++ ProjectShop_WebApi/Program.cs
@@ -1,12 +1,27 @@
 var builder = WebApplication.CreateBuilder(args);
 
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
+
 // Add services to the container.
 
 builder.Services.AddControllers();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+                      });
+});
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+app.UseCors(MyAllowSpecificOrigins);
 
 app.UseAuthorization();
 
